bff90c3fac5ac6702d906d690119a3dbe2f7c5a8
douarmoua <mouadouarzong@gmail.com>
2023-07-26 11:02:05 -0500

    add form and input material module

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
 
-const MaterialComponents:any[] = []
+const MaterialComponents:any[] = [
+  MatFormFieldModule,
+  MatInputModule
+]
 @NgModule({
   imports: [MaterialComponents],
   exports: [MaterialComponents]
