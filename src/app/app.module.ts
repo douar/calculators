186d3f524c52f61924e2df05325e243f7bf5661a
douarmoua <mouadouarzong@gmail.com>
2023-07-26 19:11:46 -0500

    first sets of input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { InvestmentEstateComponent } from './components/investment-estate/invest
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from "./material.module";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { MaterialModule } from "./material.module";
     BrowserModule,
     NgbModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
