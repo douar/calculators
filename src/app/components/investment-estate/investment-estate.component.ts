421531e52e10ea28620e46d06368bcba2e9f116e
douarmoua <mouadouarzong@gmail.com>
2023-07-26 22:07:59 -0500

    get the sum

--- src/app/components/investment-estate/investment-estate.component.ts
+++ src/app/components/investment-estate/investment-estate.component.ts
@@ -9,9 +9,17 @@ import {IBaseNumbers} from "../../interface/IBaseNumbers";
 export class InvestmentEstateComponent {
 
   baseNumbers: IBaseNumbers = {}
+  sumBaseNumbers: number = 0
   constructor() {}
 
   consoleChange(){
     console.log(this.baseNumbers)
+    let nums = Object.values(this.baseNumbers)
+    let sum = 0
+    for(let i = 0; i < nums.length; i++){
+      sum += nums[i];
+    }
+    this.sumBaseNumbers = sum
+    console.log(this.sumBaseNumbers)
   }
 }
